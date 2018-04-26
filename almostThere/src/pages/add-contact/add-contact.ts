a44dc6479087f3d6b44fb082fcc559081580ae95
emott <emott7@gmail.com>
2018-04-26 14:30:20 -0400

    link home page to trip setup page. rename blankApp to almostThere

---
+++ almostThere/src/pages/add-contact/add-contact.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-contact',
+  templateUrl: 'add-contact.html'
+})
+export class AddContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ blankApp/src/pages/setup-trip/setup-trip.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-setup-trip',
+  templateUrl: 'setup-trip.html'
+})
+export class SetupTripPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
