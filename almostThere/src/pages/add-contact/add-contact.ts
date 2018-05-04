005ae05ace1df931e62d124dc459dbf3f881d74d
derejea2 <dalemu@terpmail.umd.edu>
2018-05-04 14:45:48 -0400

    Made changes in add conatct.html/ts

--- almostThere/src/pages/add-contact/add-contact.ts
+++ almostThere/src/pages/add-contact/add-contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup,AbstractControl } from '@angular/forms';
 import {HomePage} from '../home/home';
 import { SetupTripPage } from '../setup-trip/setup-trip';
 import { ModalController, NavParams } from 'ionic-angular';
@@ -10,11 +11,33 @@ import { ModalController, NavParams } from 'ionic-angular';
   templateUrl: 'add-contact.html'
 })
 export class AddContactPage {
+	
+	private contacts : FormGroup;
+	name:AbstractControl;
+	address:AbstractControl;
+	phone:AbstractControl;
+	
 
-  constructor(public navCtrl: NavController) {
+ 	constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
 
-  }
+		  	this.contacts = this.formBuilder.group({
+		  		name: ['', Validators.required],
+		  		address: ['', Validators.required],
+		  		phone: ['', Validators.required],
+		  		
+		  	});
+
+		  	this.name = this.contacts.controls['name'];
+		  	this.address = this.contacts.controls['address'];
+		  	this.phone = this.contacts.controls['phone'];
+		  	
 
 
 
+	}
+  	contactForm(){
+  		console.log(this.contacts.value);
+
+  	
+  	}
 }
